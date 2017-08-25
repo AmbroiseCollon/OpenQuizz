bdcf5b65ee23818c708af1e2a1ff0898418f0286
AmbroiseCollon <ambroise.collon@gmail.com>
2017-08-25 17:16:29 +0200

    Fixed Icon

--- OpenQuizz/QuestionView.swift
+++ OpenQuizz/QuestionView.swift
@@ -24,10 +24,15 @@ class QuestionView: UIView {
         switch style {
         case .correct:
             backgroundColor = UIColor(red: 200.0/255.0, green: 236.0/255.0, blue: 160.0/255.0, alpha: 1)
+            icon.image = #imageLiteral(resourceName: "Icon Correct")
+            icon.isHidden = false
         case .incorrect:
             backgroundColor = UIColor(red: 243.0/255.0, green: 135.0/255.0, blue: 148.0/255.0, alpha: 1)
+            icon.image = #imageLiteral(resourceName: "Icon Error")
+            icon.isHidden = false
         case .standard:
             backgroundColor = UIColor(red: 191.0/255.0, green: 196.0/255.0, blue: 201.0/255.0, alpha: 1)
+            icon.isHidden = true
         }
     }
 
